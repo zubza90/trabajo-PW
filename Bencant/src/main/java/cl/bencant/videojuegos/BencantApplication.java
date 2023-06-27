1c36d0ad0ac889972ec2f382b0e10a259c13ec01
benjamin <be.candia@duocuc.cl>
2023-06-27 19:46:20 -0400

    intellij add

---
+++ Bencant/src/main/java/cl/bencant/videojuegos/BencantApplication.java
@@ -0,0 +1,13 @@
+package cl.bencant.videojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BencantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BencantApplication.class, args);
+	}
+
+}
---
+++ Bencant/src/test/java/cl/bencant/videojuegos/BencantApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bencant.videojuegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BencantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
